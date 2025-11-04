807996f123715f87a664232d5094abedbaa82313
MasterEJ-Robotics <ejohn8915@gmail.com>
2025-11-03 23:48:52 -0500

    initial commit

---
+++ data_cleaning.R
@@ -0,0 +1,84 @@
+library(dplyr)
+library(readr)
+library(ggplot2)
+df <- read.csv("/Users/ethanjohn/Desktop/Data Science/Projects/MH_Dashboard2/Students Social Media Addiction copy 2.csv")
+
+glimpse(df)
+
+#Check missing values
+missing_vals = sum(is.na(df))
+print(missing_vals)
+
+#Create new derived features (feature engineering)
+
+#Time & usage related features
+
+df_2 <- df %>%
+  mutate(
+    
+
+    #Balance_Score = Sleep_Hours_Per_Night - Avg_Daily_Usage_Hours,
+
+    #Usage_Sleep_Ratio = Avg_Daily_Usage_Hours / Sleep_Hours_Per_Night,
+
+    #Usage_Efficiency_Level = case_when(
+        #Usage_Sleep_Ratio < 0.3 ~ "Healthy",
+        #Usage_Sleep_Ratio < 0.6 ~ "Moderate",
+        #TRUE ~ "Unhealthy"
+    #),
+    
+    #Platform_Usage = paste(Most_Used_Platform, "-", Usage_Level),
+
+    #Lifestyle_Balance_Score = (Sleep_Hours_Per_Night - Avg_Daily_Usage_Hours) * Mental_Health_Score,
+
+    # Note, maybe use ML to predict factors that influence affects academic performance
+    Usage_Hrs_Level = case_when(
+        Avg_Daily_Usage_Hours < 2 ~ "Low",
+        Avg_Daily_Usage_Hours < 5 ~ "Medium",
+        TRUE ~ "High"
+    ),
+
+    Sleep_Deficit = 8 - Sleep_Hours_Per_Night,
+
+    Platform_Categories = case_when(
+      Most_Used_Platform %in% c("YouTube", "TikTok") ~ "Video-Based",
+      Most_Used_Platform %in% c("LinkedIn") ~ "Professional Networking",
+      Most_Used_Platform %in% c("Instagram", "Facebook", "VKontakte") ~ "Content-Sharing",
+      Most_Used_Platform %in% c("Twitter") ~ "Microblogging",
+      Most_Used_Platform %in% c("Snapchat", "WhatsApp", "WeChat", "LINE", "KakaoTalk") ~ "Messaging",
+      TRUE ~ "Other"
+    ),
+
+    Addicted_Score_Levels = case_when(
+      Addicted_Score < 3 ~ "Low",
+      Addicted_Score < 6 ~ "Moderate",
+      TRUE ~ "High"
+    ),
+
+    Wellbeing_Score = Mental_Health_Score + Sleep_Hours_Per_Night - Addicted_Score,
+
+    Relationship_Status_Binary = case_when(
+      Relationship_Status == "Single" ~ 1,
+      Relationship_Status == "In Relationship" ~ 2,
+      Relationship_Status == "Complicated" ~ 3
+
+    ),
+
+    Affects_Academic_Performance_Encoded = case_when(
+      Affects_Academic_Performance == "Yes" ~ 1,
+      Affects_Academic_Performance == "No" ~ 0
+
+    ),
+
+    Academic_Level_Encoded = case_when(
+      Academic_Level == "High School" ~ 1,
+      Academic_Level == "Undergraduate" ~ 2,
+      Academic_Level == "Graduate" ~ 3
+
+    )
+
+
+  )
+
+head(df_2)
+write.csv(df_2, "/Users/ethanjohn/Desktop/Data Science/Projects/MH_Dashboard2/Students_Social_Media_Addiction_FE.csv", row.names = FALSE)
