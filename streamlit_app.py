Fixed bug in formula and developed dashboard

streamlit_app.py
@@ -1,72 +1,66 @@
 import streamlit as st
 import joblib
+import numpy as np
 
-
-model = joblib.load("logistic_model.pkl")
-scaler = joblib.load("scaler.pkl")
-
+bias_params = joblib.load("bias_params.pkl")
+MH_bias = bias_params["MH_Bias_Factor"]
+A_bias = bias_params["AS_Bias_Factor"]
 
 st.title("Student Mental Health Dashboard")
-st.write("This interactive web app hosts a model that can be used in identifying whether or not one's social media habits has an impact on their academic performance. ")
+st.write("Description goes here: ")
 
-
-
-col1,col2 = st.columns(2)
+col1, col2 = st.columns(2)
 
 with col1:
-    sleep_hrs = st.slider(
-        label="Input # hours of sleep you get in general",
-        min_value=0,
-        max_value=24,
-        value=7)
 
-    usage_hrs = st.slider(
-        label="Input # hours you spend on social media in general",
+    sleep_hrs = st.slider(
+        label="Hours of sleep per night",
         min_value=0,
-        max_value=24,
-        value=3)
+        max_value=12,
+        value=7
+    )
+    
+    usage_hrs = st.slider(
+        label="Hours spent on social media per day",
+        min_value=0,
+        max_value=12,
+        value=3
+    )
 
     conflicts_count = st.slider(
-        label="Input # conflicts you've had on social media",
+        label="Number of social media conflicts",
         min_value=0,
-        max_value=100,
-        value=0) # Check what to set max value
-
-    academic_level = st.selectbox(
-        label="What is your academic level?", 
-        options=["High School", "Undergraduate", "Postgraduate"], 
-        index=None,
-        placeholder="Select Academic Level")
-
-academic_mapping = {
-    "High School": 0,
-    "Undergraduate": 1,
-    "Postgraduate": 2
-}
-
-def calc_mh_score(usage_hrs, sleep_hrs, conflicts):
-    score = (1.2 * sleep_hrs - 
-    0.7 * usage_hrs - 
-    0.3 * conflicts
+        max_value=50,
+        value=0
     )
 
-    return round(max(min(score,10), 0), 2)
-
-def calc_addicted_score(usage_hrs, sleep_hrs, conflicts):
-    score = (
-        0.6 * usage_hrs + 
-        0.25 * conflicts + 
-        0.5 * max(0, 8 - sleep_hrs)
-
+def calc_mh_score(usage_hrs,sleep_hrs,conflicts):
+    raw = (
+        0.6 * sleep_hrs - 
+        0.25 * usage_hrs - 
+        0.15 * conflicts
     )
-    return round(min(score,10),2)
 
-mental_health_score_pred = calc_mh_score(usage_hrs,sleep_hrs,conflicts_count)
+    return np.clip(raw + MH_bias, 0, 10)
+
+def calc_addicted_score(usage_hrs,sleep_hrs,conflicts):
+    raw = (
+        0.7 * usage_hrs +
+        0.15 * conflicts + 
+        0.15 * max(0, 8-sleep_hrs)
+    )
+
+    return np.clip(raw + A_bias, 0, 10)
+
+mh_score_pred = calc_mh_score(usage_hrs,sleep_hrs,conflicts_count)
+
 addicted_score_pred = calc_addicted_score(usage_hrs,sleep_hrs,conflicts_count)
 
 st.subheader("Estimated Scores")
-st.metric("Estimated Addiction Score (0-10)", addicted_score_pred)
-st.metric("Estimated Mental Health Score (0-10)", mental_health_score_pred)
-
-
-st.caption("NOTE - This is not meant to serve medical advice/diagnosis")
+st.metric(
+    "Estimated Addiction Score (0-10)", round(addicted_score_pred,2)
+)
+st.metric(
+    "Estimated Mental Health Score Score (0-10)", round(mh_score_pred,2)
+)
+st.caption("This tool is for educational purposes, not medical advice. ")